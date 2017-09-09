9dc86823f18c617e0e9bbc01b394e237272b4ecb
sbarnwell88 <susy6688@gmail.com>
2017-09-09 17:00:10 -0400

    added favorites controller and tested with postman

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,3 +1,19 @@
 class Api::FavoritesController < ApplicationController
-    before_action :authenticate_user!
+    # before_action :authenticate_user!
+
+    def index
+        @favorites = Favorite.all 
+        render json: @favorites
+    end
+
+    def create
+        boardgame_id = params[:boardgame_id]
+        user_id = params[:user_id]
+        @favorite = Favorite.create!(user_id: user_id, boardgame_id: boardgame_id)
+    end
+
+    def destroy
+        @favorite = Favorite.find params[:id]
+        @favorite.destroy
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
-    resources :boardgames
     resources :favorites
+    resources :boardgames
+    resources :top_rated
     end
   root to: "boardgames#index"
 end
